2b6c64ddc388a7ac8086d3f33d8bf296fc553552
3220110485 <zihan.22@intl.zju.edu.cn>
2023-03-22 20:11:27 +0800

    practical5

---
+++ Practical5/variables.py
@@ -0,0 +1,22 @@
+# 3.1
+a = -3.19
+b = -118.24
+c = 116.39
+d = abs(a-b)
+print(d)
+e = abs(a-c)
+print(e)
+if d > e:
+    print("Rob	travel	further	to	Haining")
+else:
+    print("Rob	travel	further	to	Los	Angeles")
+
+# 3.2
+X = False
+Y = True
+W = X and Y
+Z = X or Y
+print(W)       # W is the answer of "x and y"
+print(Z)        # Z is the answer of "x or y"
+
+
